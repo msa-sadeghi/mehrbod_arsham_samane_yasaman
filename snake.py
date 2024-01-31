78eed6038960dd28f50fe70caf31054db69f24e8
msa-sadeghi <sadeghi.msai@gmail.com>
2024-01-31 17:57:37 +0330

    Update snake.py
    score added
    snake tails list added

--- snake.py
+++ snake.py
@@ -20,16 +20,52 @@ def move():
         y = snake_head.ycor()
         y += 20
         snake_head.sety(y)
+    if snake_head.direction == "down":
+        y = snake_head.ycor()
+        y -= 20
+        snake_head.sety(y)
+    if snake_head.direction == "left":
+        x = snake_head.xcor()
+        x -= 20
+        snake_head.setx(x)
+    if snake_head.direction == "right":
+        x = snake_head.xcor()
+        x += 20
+        snake_head.setx(x)
 def go_up():
     snake_head.direction = "up"
+def go_down():
+    snake_head.direction = "down"
+def go_left():
+    snake_head.direction = "left"
+def go_right():
+    snake_head.direction = "right"
 snake_head = create_turtle("square", "green")
 snake_head.direction = ""
 snake_food = create_turtle("circle", "red")
 change_food_pos()
 screen.listen()
 screen.onkeypress(go_up,"Up")
+screen.onkeypress(go_down,"Down")
+screen.onkeypress(go_left,"Left")
+screen.onkeypress(go_right,"Right")
+
+score = 0
+scoreboard = create_turtle("square", "white")
+scoreboard.goto(0, 260)
+scoreboard.ht()
+
+
+all_tails = []
 running = True
 while running == True:
+    scoreboard.clear()
+    scoreboard.write(f"Score:{score}", font=("arial",22), align="center")
     screen.update()
+    if snake_head.distance(snake_food) < 20:
+        change_food_pos()
+        score += 1
+        new_tail = create_turtle("square","darkgreen")
+        all_tails.append(new_tail)
     move()
     time.sleep(0.2)
