278e5b31f5182d093a602518dcf3cf731d31d65b
msa-sadeghi <sadeghi.msai@gmail.com>
2024-03-17 16:17:14 +0330

    a

--- pygame_course/first_game/main.py
+++ pygame_course/first_game/main.py
@@ -4,17 +4,24 @@ pygame.init()
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+FPS = 60
+clock = pygame.time.Clock()
 fox_image = pygame.image.load("fox.png")
+fox_image = pygame.transform.scale(fox_image, (256,256))
 fox_rect = fox_image.get_rect()
-fox_rect.topleft = (0, SCREEN_HEIGHT - 512)
-
+fox_rect.bottomright = (SCREEN_WIDTH, SCREEN_HEIGHT)
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        fox_rect.x -= 5
+    if keys[pygame.K_RIGHT]:
+        fox_rect.x += 5
     screen.fill((205, 0,100)) 
     screen.blit(fox_image, fox_rect)       
     pygame.display.update()
+    clock.tick(FPS)
