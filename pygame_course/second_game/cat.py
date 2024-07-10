8fcbacf9f0a97b8c99e7f30012aea017f7600989
msa-sadeghi <sadeghi.msai@gmail.com>
2024-07-10 17:55:39 +0330

    a

--- pygame_course/second_game/cat.py
+++ pygame_course/second_game/cat.py
@@ -10,6 +10,30 @@ class Cat:
         self.image = self.all_images[0]
         self.rect = self.image.get_rect()
         self.rect.topleft = (x,y)
+        self.image_number = 0
+        self.last_update_time = 0
         
     def draw(self, screen):
         screen.blit(self.image, self.rect)
+        
+    def animation(self):
+        if pygame.time.get_ticks() - self.last_update_time > 200:
+            self.last_update_time = pygame.time.get_ticks()
+            self.image_number += 1
+        if self.image_number >= len(self.all_images):
+            self.image_number = 0
+        self.image = self.all_images[self.image_number]
+        
+    def move(self):
+        dx = 0
+        dy = 0
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            dx -= 5
+        if keys[pygame.K_RIGHT]:
+            dx += 5
+            
+        self.rect.x += dx
+        self.rect.y += dy
+            
+        
--- pygame_course/second_game/main.py
+++ pygame_course/second_game/main.py
@@ -1,9 +1,12 @@
 import pygame
 from cat import Cat
+from pygame.time import Clock
 my_cat = Cat(100, 200)
 screen_width = 480
 screen_height = 360
 white = (255, 255, 255)
+FPS = 60
+c = Clock()
 screen = pygame.display.set_mode((screen_width, screen_height))
 running = True
 while running:
@@ -11,5 +14,8 @@ while running:
         if ev.type == pygame.QUIT:
             running = False
     screen.fill(white) 
-    my_cat.draw(screen)       
+    my_cat.draw(screen)
+    my_cat.animation()  
+    my_cat.move()     
     pygame.display.update()
+    c.tick(FPS)
