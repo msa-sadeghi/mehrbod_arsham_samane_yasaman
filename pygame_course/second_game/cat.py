c237395e33b14d0880a1989d5d66262b04a69ed4
msa-sadeghi <sadeghi.msai@gmail.com>
2024-07-17 18:00:50 +0330

    a

--- pygame_course/second_game/cat.py
+++ pygame_course/second_game/cat.py
@@ -2,36 +2,57 @@ import pygame
 
 class Cat:
     def __init__(self, x,y):
-        self.all_images = []
+        self.right_images = []
+        self.left_images = []
         for i in range(1,3):
             img = pygame.image.load(f"costume{i}.svg")
-            self.all_images.append(img)
+            self.right_images.append(img)
+            img = pygame.transform.flip(img, True, False)
+            self.left_images.append(img)
             
-        self.image = self.all_images[0]
+            
+        self.image = self.right_images[0]
         self.rect = self.image.get_rect()
         self.rect.topleft = (x,y)
         self.image_number = 0
         self.last_update_time = 0
+        self.direction = 1
+        self.moving = False
         
     def draw(self, screen):
+        
         screen.blit(self.image, self.rect)
         
     def animation(self):
-        if pygame.time.get_ticks() - self.last_update_time > 200:
-            self.last_update_time = pygame.time.get_ticks()
-            self.image_number += 1
-        if self.image_number >= len(self.all_images):
-            self.image_number = 0
-        self.image = self.all_images[self.image_number]
+        if self.moving:
+            if pygame.time.get_ticks() - self.last_update_time > 200:
+                self.last_update_time = pygame.time.get_ticks()
+                self.image_number += 1
+            if self.image_number >= len(self.right_images):
+                self.image_number = 0
+        if self.direction == 1:
+            self.image = self.right_images[self.image_number]
+        elif self.direction == -1:
+            self.image = self.left_images[self.image_number]
         
     def move(self):
         dx = 0
         dy = 0
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
+            self.flip = True
+            self.moving = True
             dx -= 5
+            self.direction = -1
         if keys[pygame.K_RIGHT]:
             dx += 5
+            self.direction = 1
+            self.moving = True
+        if not keys[pygame.K_LEFT] and not keys[pygame.K_RIGHT]:
+            self.moving = False
+            
+            
+            
             
         self.rect.x += dx
         self.rect.y += dy
